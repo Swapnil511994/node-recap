9feb891221f7bc6165367012c194a90440f60623
Swapnil511994 <Swapnil511994@gmail.com>
2024-03-26 15:55:21 +0530

    Added Rabbit MQ

--- index.js
+++ index.js
@@ -3,15 +3,39 @@ import bodyParser from "body-parser";
 import registerRoutes from "./routes.js";
 import { config } from "./config.js";
 
+import amqp from "amqplib/callback_api.js";
+import logger from "./logger.js";
+
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 registerRoutes(app);
 app.listen(config.ApplicationConfig.port, () => {
+  try {
+    amqp.connect("amqp://localhost", (err, conn) => {
+      if (err) {
+        throw err;
+      }
+      conn.createChannel((channelCreationError, channel) => {
+        if (channelCreationError) throw channelCreationError;
+        const queue = "helloWorldQueue";
+        const message = "Hello World";
+        channel.assertQueue(queue, { durable: false });
+        channel.sendToQueue(queue, Buffer.from(message));
+        console.log(" [x] Sent %s", message);
+      });
+    });
+  } catch (error) {
+    logger.error(
+      "Unable to initialize rabbit mq: " + error?.message
+        ? error.message
+        : "Unknown Error"
+    );
+  }
   console.log(`Server Started 🚀 at Port: ${config.ApplicationConfig.port}`);
 });
